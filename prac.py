d946e8885fe3791f538cc6570ae261c9040f8aeb
livingDead1023 <banerjeeritashish@gmail.com>
2025-08-15 17:28:01 +0000

    new file:   .vscode/settings.json 	modified:   prac.py 	new file:   util.py

--- prac.py
+++ prac.py
@@ -1,1 +1,5 @@
-#
+# Sum of first 10 natural numbers using a for loop
+total = 0
+for i in range(1, 11):
+	total += i
+print("Sum of first 10 natural numbers:", total)
---
+++ util.py
@@ -0,0 +1,8 @@
+def bubble_sort(l):
+    for i in range(0, len(l)):
+        for j in range(0,len(l)-i-1):
+            if l[j]>l[j+1]:
+                temp = l[j]
+                l[j] = l[j+1]
+                l[j+1] = temp
+    return l
