d9dde5bae14a44b7f3bfc0a944f5cf630e6195ad
livingDead1023 <banerjeeritashish@gmail.com>
2025-08-15 07:43:39 +0000

    modified:   prac.py

--- prac.py
+++ prac.py
@@ -1,6 +1,6 @@
 name = input("Enter your name: ")
 fname = input("Enter your father's name: ")
 grade = int(input("Enter your class: "))
-sname = input("Enter your school name:")
+sname = input("Enter your school name: ")
 
-print(f"Name:{name}\nFather's Name:{fname}\nClass:{grade}\nSchool Name:{sname}")
+print(f"Name: {name}\nFather's Name: {fname}\nClass: {grade}\nSchool Name: {sname}")
