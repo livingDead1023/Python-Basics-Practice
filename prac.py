7efc602bfea625999482143d96ca385a03a889d1
livingDead1023 <banerjeeritashish@gmail.com>
2025-08-15 14:49:59 +0000

    deleted:    .venvmain/bin/python 	deleted:    .venvmain/bin/python3 	deleted:    .venvmain/bin/python3.12 	deleted:    .venvmain/lib64 	deleted:    .venvmain/pyvenv.cfg 	modified:   prac.py

--- prac.py
+++ prac.py
@@ -1,8 +1,1 @@
-l=float(input("Length of the cuboid:"))
-b=float(input("Breadth ofthe cuboid:"))
-h=float(input("Height of the cuboid:"))
-
-TSA = 2*(l*b + b*h + l*h)
-v=l*b*h
-print("Total Surface Area of cuboid =",TSA)
-print("Volume of cuboid =",v)
+#
